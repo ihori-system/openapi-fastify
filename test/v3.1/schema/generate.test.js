f45cb8971f0890fa13f118e50793da5ee0240ea7
Issei Horie <issei.horie@ihori.co.jp>
2023-06-17 06:46:12 +0900

    install typescript

--- test/v3.1/schema/generate.test.js
+++ test/v3.1/schema/generate.test.js
@@ -1,11 +1,24 @@
 const assert = require('node:assert').strict
+const fs = require('node:fs')
+const path = require('node:path')
 const test = require('node:test')
+const ts = require('typescript')
 const {
   generate
 } = require('../../../lib/v3.1/schema/generate')
 
+const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed })
+
+const equal = async (t, target) =>
+  await t.test(target, () => assert.equal(
+    printer.printFile(generate(require(path.join('../fixtures/json', `${target}.json`)))),
+    fs.readFileSync(path.join(__dirname, '../fixtures/schema', `${target}.ts`), 'utf8')
+  ))
+
 test('generate', async (t) => {
-  await t.test('', () => {
-    assert.equal(generate(), undefined)
-  })
+  /**
+   * Examples in OAI/OpenAPI-Specification repository
+   */
+  // https://github.com/OAI/OpenAPI-Specification/blob/main/examples/v3.1/non-oauth-scopes.json
+  await equal(t, 'non-oauth-scopes')
 })

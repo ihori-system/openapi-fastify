f4f466a13573b14c2ecfa5e497f099104f0118a7
Issei Horie <issei.horie@ihori.co.jp>
2023-06-16 23:44:47 +0900

    setup package.json

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# openapi-fastify
+openapi-fastify
+===
+
 Command line tool for Fastify, Generate schemas from OpenAPI JSON.
+
+## Usage
+
+```
+npx openapi-fastify --input <file> --output <path> [options]
+```
---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+const pc = require('picocolors')
+const argv = require('yargs-parser')(process.argv.slice(2))
+const packageJson = require('./package.json')
+
+const HELP = `
+openapi-fastify - command line Fastify schema generator [version ${packageJson.version}]
+
+${pc.bold('USAGE')}
+  $ openapi-fastify --input <file> --output <path> [options]
+
+${pc.bold('OPTIONS')}
+  --help      help about this command
+  --input     OAS file
+  --omit      omit schema or type <schema | type>
+  --output    output path
+  --type      OAS file type <json | yaml>
+`
+
+const buildErrorMessage = (error) => `
+${pc.cyan('[openapi-fastify]')} ${pc.bgRed('ERROR')} ${error}
+`
+
+const main = async () => {
+  if (argv.help) {
+    return console.info(HELP)
+  }
+}
+
+main()
+  .catch(error => console.error(buildErrorMessage(error)))

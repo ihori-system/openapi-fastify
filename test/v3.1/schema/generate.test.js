54c8b7a3dc2b92c8075a81793b217667cb7cf85c
Issei Horie <issei.horie@ihori.co.jp>
2023-06-17 23:33:43 +0900

    test non-oauth-scopes.json

--- test/v3.1/schema/generate.test.js
+++ test/v3.1/schema/generate.test.js
@@ -9,16 +9,21 @@ const {
 
 const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed })
 
-const equal = async (t, target) =>
-  await t.test(target, () => assert.equal(
-    printer.printFile(generate(require(path.join('../fixtures/json', `${target}.json`)))),
-    fs.readFileSync(path.join(__dirname, '../fixtures/schema', `${target}.ts`), 'utf8')
-  ))
+test('generate from json', async (t) => {
+  const equal = async (t, target) =>
+    await t.test(target, () => assert.equal(
+      printer.printFile(generate(path.join('test/v3.1/fixtures/json', `${target}.json`))),
+      fs.readFileSync(path.join(__dirname, '../fixtures/schema', `${target}.ts`), 'utf8')
+    ))
 
-test('generate', async (t) => {
   /**
    * Examples in OAI/OpenAPI-Specification repository
    */
   // https://github.com/OAI/OpenAPI-Specification/blob/main/examples/v3.1/non-oauth-scopes.json
   await equal(t, 'non-oauth-scopes')
+
+  /**
+   * Custom fixtures
+   */
+  await equal(t, 'openapi-object-without-paths-object')
 })

d5fcf46017d23863b937cdcc4e1232eef927c65c
Issei Horie <issei.horie@ihori.co.jp>
2023-06-18 18:03:10 +0900

    support default field

--- test/v3.1/schema/parse.test.js
+++ test/v3.1/schema/parse.test.js
@@ -1,11 +1,11 @@
-// const assert = require('node:assert').strict
-// const test = require('node:test')
-// const {
-//   parsePathsObject
-// } = require('../../../lib/v3.1/schema/parse')
+const assert = require('node:assert').strict
+const test = require('node:test')
+const {
+  valueToLiteral
+} = require('../../../lib/v3.1/schema/parse')
 
-// test('generate', async (t) => {
-//   await t.test('parsePathsObject', () => {
-//     assert.equal(parsePathsObject(), undefined)
-//   })
-// })
+test('valueToLiteral', async (t) => {
+  await t.test('unexpected value', () => {
+    assert.throws(() => valueToLiteral(Symbol('')))
+  })
+})

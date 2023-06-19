05b87201b55e736468be0ea0f6c705d93314b5b4
Issei Horie <issei.horie@ihori.co.jp>
2023-06-19 17:17:37 +0900

    add skelton code for v3.0

---
+++ test/v3.0/type/generate.test.js
@@ -0,0 +1,23 @@
+const assert = require('node:assert').strict
+const fs = require('node:fs')
+const path = require('node:path')
+const test = require('node:test')
+const ts = require('typescript')
+const {
+  generate
+} = require('../../../lib/v3.0/type/generate')
+
+const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed })
+
+test('generate from json', async (t) => {
+  const equal = async (t, target) =>
+    await t.test(target, () => assert.equal(
+      printer.printFile(generate(path.join('test/v3.0/fixtures/json', `${target}.json`))),
+      fs.readFileSync(path.join(__dirname, '../fixtures/type', `${target}.ts`), 'utf8')
+    ))
+
+  /**
+   * Examples in OAI/OpenAPI-Specification repository
+   */
+  await equal(t, 'api-with-examples')
+})

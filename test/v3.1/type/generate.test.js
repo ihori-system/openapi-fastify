17d2a3d0d6e2e126e5d09527a56e6367e58ece96
Issei Horie <issei.horie@ihori.co.jp>
2023-06-19 12:55:37 +0900

    added test case for type generation

--- test/v3.1/type/generate.test.js
+++ test/v3.1/type/generate.test.js
@@ -50,6 +50,8 @@ test('generate from json', async (t) => {
   await equal(t, 'responses-object-example')
   // https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#reference-object-example
   await equal(t, 'reference-object-example')
+  // https://github.com/OAI/OpenAPI-Specification/blob/main/versions/3.1.0.md#primitive-sample
+  await equal(t, 'schema-object-example-primitive-sample')
 
   /**
    * Custom fixtures

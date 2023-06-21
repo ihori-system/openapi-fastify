dc6f5294515cd9336686eff01bacddc65d7c2abd
Issei Horie <issei.horie@ihori.co.jp>
2023-06-21 13:26:53 +0900

    parse responses object

--- test/v3.0/type/generate.test.js
+++ test/v3.0/type/generate.test.js
@@ -31,4 +31,7 @@ test('generate from json', async (t) => {
    */
   await throws(t, 'openapi-object-without-info-object')
   await throws(t, 'openapi-object-without-paths-object')
+  await throws(t, 'operation-object-without-responses')
+  await equal(t, 'response-object-without-content')
+  await throws(t, 'response-object-without-description')
 })

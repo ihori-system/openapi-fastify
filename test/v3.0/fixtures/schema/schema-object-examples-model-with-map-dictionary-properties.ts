b1176bca26f26fb666b02597acf5a2224abb2c54
Issei Horie <issei.horie@ihori.co.jp>
2023-06-22 15:52:55 +0900

    added test case

---
+++ test/v3.0/fixtures/schema/schema-object-examples-model-with-map-dictionary-properties.ts
@@ -0,0 +1,13 @@
+export const getUser = {
+    response: {
+        200: {
+            content: {
+                "application/json": {
+                    schema: {
+                        type: "object"
+                    }
+                }
+            }
+        }
+    }
+};
---
+++ test/v3.0/fixtures/type/schema-object-examples-model-with-map-dictionary-properties.ts
@@ -0,0 +1,2 @@
+export interface getUserReply200 {
+}

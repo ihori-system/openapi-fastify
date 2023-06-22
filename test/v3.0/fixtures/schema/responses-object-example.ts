a02d81c8901c6de7c45bcaf5eae8eb3f8a1326d2
Issei Horie <issei.horie@ihori.co.jp>
2023-06-22 15:19:57 +0900

    added test case

---
+++ test/v3.0/fixtures/schema/responses-object-example.ts
@@ -0,0 +1,22 @@
+export const index = {
+    response: {
+        200: {
+            content: {
+                "application/json": {
+                    schema: {
+                        type: "object"
+                    }
+                }
+            }
+        },
+        default: {
+            content: {
+                "application/json": {
+                    schema: {
+                        type: "object"
+                    }
+                }
+            }
+        }
+    }
+};
---
+++ test/v3.0/fixtures/type/responses-object-example.ts
@@ -0,0 +1,8 @@
+export interface indexReply200 {
+}
+export interface indexReplyDefault {
+}
+export interface PetSchema {
+}
+export interface ErrorModelSchema {
+}
